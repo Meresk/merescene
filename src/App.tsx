61715d07a71a7c565a073b5af12d0fc6d8c2737f
Meresk <Yakovx95@mail.ru>
2026-03-02 17:37:52 +0300

    light start

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <span>mere-scene</span>
-    </>
-  )
+    <div className="app">
+      <h1 className="title">mere _</h1>
+
+      <div className="preview-wrapper">
+        <iframe
+          src="http://localhost:5173"
+          className="preview-iframe"
+        />
+        <div
+          className="overlay"
+          onClick={() => window.open("http://localhost:5173", "_blank")}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
